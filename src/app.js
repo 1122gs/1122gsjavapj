91c329ccc3cdbceb44d9fb2136450ccb24a633cf
1122gs <GORDON.SMITHJR@hotmail.com>
2024-11-05 20:24:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+function shuffle(a, b) {
+  return Math.floor(Math.random() * (a - b) + b);
+}
+function random(word) {
+  let max = word.length;
+  let min = 0;
+  let random = shuffle(max, min);
+  return word[random];
+}
 window.onload = function() {
   //write your code here
+
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    random(who) +
+    " " +
+    random(action) +
+    " " +
+    random(what) +
+    " " +
+    random(when);
 };
